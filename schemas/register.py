2b27add6c949190e0f14cba775c7d173bf9f6d99
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-05-23 14:07:05 +0300

    # каталог для схем API

---
+++ schemas/register.py
@@ -0,0 +1,29 @@
+schema_post_register_successful = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "integer"
+    },
+    "token": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "id",
+    "token"
+  ]
+}
+
+schema_post_register_unsuccessful = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "error": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "error"
+  ]
+}
